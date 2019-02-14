899114453fb5d34f943fd8b26073e11f76380ca4
Cooper Anderson <cooper@cooperstuff.ninja>
2019-02-14 15:09:16 -0500

    Implement TreeSetSort

--- src/TreeSetSort.java
+++ src/TreeSetSort.java
@@ -1,5 +1,24 @@
-public class TreeSetSort {
-	public static void sort(Integer[] array) {
+import java.util.HashMap;
+import java.util.TreeSet;
 
+class TreeSetSort {
+	static void sort(Integer[] array) {
+		HashMap<Integer, Integer> counts = new HashMap<>();
+		TreeSet<Integer> set = new TreeSet<>();
+		for (Integer item : array) {
+			if (!set.add(item)) {
+				if (!counts.containsKey(item)) counts.put(item, 2);
+				else counts.put(item, counts.remove(item) + 1);
+			}
+		}
+		int index = 0;
+		for (Integer item : set) {
+			try {
+				Integer count = counts.get(item);
+				for (int i = 0; i < count; i++) array[index++] = item;
+			} catch (NullPointerException e) {
+				array[index++] = item;
+			}
+		}
 	}
 }
