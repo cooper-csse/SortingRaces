0eac37db47e0eb3f28e817065432f7a09df6f439
Cooper Anderson <cooper@cooperstuff.ninja>
2019-02-14 18:36:55 -0500

    Implement QuickSort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,5 +1,23 @@
-public class QuickSort {
-	public static void sort(int[] array) {
+class QuickSort {
+	static void sort(int[] array) {
+		sort(array, 0, array.length - 1);
+	}
 
+	private static void sort(int[] array, int left, int right) {
+		if (left >= right) return;
+		int pivot = array[left + (right - left) / 2];
+		int i = left, j = right;
+		while (i <= j) {
+			while (array[i] < pivot) i++;
+			while (array[j] > pivot) j--;
+			if (i <= j) {
+				int item = array[i];
+				array[i] = array[j];
+				array[j] = item;
+				i++; j--;
+			}
+		}
+		sort(array, left, i - 1);
+		sort(array, i, right);
 	}
 }
